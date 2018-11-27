9fceef5c4236e92e024b7a2c5063660c2ba4e125
Samuel Colville <S00182360@mail.itsligo.ie>
2018-11-27 15:27:44 +0000

    Movies displaying in main window

--- FOOP_Lab9/MainWindow.xaml.cs
+++ FOOP_Lab9/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,51 @@ namespace FOOP_Lab9
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Movie> movieList = new ObservableCollection<Movie>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Movie loading
+            Movie M1 = new Movie("Gone Girl", "Drama", 5);
+            movieList.Add(M1);
+            Movie M2 = new Movie("Tomb Raider", "Action", 5);
+            movieList.Add(M2);
+            Movie M3 = new Movie("Harry Potter and the Prizoner of Azkaban", "Fantasy", 4);
+            movieList.Add(M3);
+
+            lbx_MovieList.ItemsSource = movieList;
+            
+
+    }
+
+        private void Btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            //Read in information
+            string title = tbx_Title.Text;
+            string genre = tbx_Genre.Text;
+            int rating = int.Parse(cbx_Rating.Text);
+
+            Movie newMovie = new Movie(title, genre, rating);
+
+            movieList.Add(newMovie);
+
+        }
+
+        private void Btn_Remove_Click(object sender, RoutedEventArgs e)
+        {
+            Movie movieSelect = lbx_MovieList.SelectedItem as Movie;
+
+            if(lbx_MovieList != null)
+            {
+                movieList.Remove(movieSelect);
+            }
         }
     }
 }
---
+++ FOOP_Lab9/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP_Lab9
+{
+    class Movie
+    {
+        private string _title;
+        private string _genre;
+        private int _rating;
+
+        public string Title { get => _title; set => _title = value; }
+        public string Genre { get => _genre; set => _genre = value; }
+        public int Rating { get => _rating; set => _rating = value; }
+
+        public Movie(string title): this(title, "Unknown", 0)
+        {
+        }
+
+        public Movie():this("Unknown")
+        {
+        }
+
+        public Movie(string title, string genre, int rating)
+        {
+            Title = title;
+            Genre = genre;
+            Rating = rating;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, Genre: {1}, Rating: {2}", Title, Genre, Rating); 
+        }
+
+        //public enum GENRE { }
+    }
+}
